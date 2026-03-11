0be78152a16d3be4569b4c4002b90df4686f142e
tuannd05 <nguyendangtuan150705@gmail.com>
2026-03-11 14:15:01 +0700

    up

---
+++ src/main/java/com/example/sof3062sd20303/Sof3062Sd20303Application.java
@@ -0,0 +1,14 @@
+package com.example.sof3062sd20303;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof3062Sd20303Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sof3062Sd20303Application.class, args);
+        System.out.println("running...");
+    }
+
+}
---
+++ src/test/java/com/example/sof3062sd20303/Sof3062Sd20303ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sof3062sd20303;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sof3062Sd20303ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
