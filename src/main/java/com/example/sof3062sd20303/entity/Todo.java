6d9c79e028370b8314525afddc08fab56b2d83c5
tuannd05 <nguyendangtuan150705@gmail.com>
2026-03-19 13:27:01 +0700

    updatevalidation

--- src/main/java/com/example/sof3062sd20303/entity/Todo.java
+++ src/main/java/com/example/sof3062sd20303/entity/Todo.java
@@ -1,6 +1,7 @@
 package com.example.sof3062sd20303.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -14,7 +15,9 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min = 3, max = 50, message = "Title must be between 3 and 50 characters")
     private String title;
+    @Size(min = 3, message = "Description should have at least 3 characters")
     private String description;
     private boolean completed;
 
